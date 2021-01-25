f5ac6a689a9ce8673d034b4a5d5233a3dd681344
sh086 <suheforvip@gmail.com>
2021-01-25 15:49:26 +0800

    v1.1 使用AdminLTE模板重写登录页面

--- myshop/src/main/java/com/shooter/funtl/module/web/controller/LoginController.java
+++ myshop/src/main/java/com/shooter/funtl/module/web/controller/LoginController.java
@@ -19,14 +19,16 @@ public class LoginController extends HttpServlet{
         //获取参数
         String loginId = req.getParameter("loginId");
         String loginPwd = req.getParameter("loginPwd");
-        //验证用户名、密码是否正确
+        //查询用户信息
         User user = userService.login(loginId, loginPwd);
         //登录失败的处理
         if(user == null){
-            req.getRequestDispatcher("/fail.jsp").forward(req,resp);
+            req.setAttribute("message","用户名或密码错误！");
+            req.getRequestDispatcher("/index.jsp").forward(req,resp);
         }
         //登录成功的处理
         else {
+            req.setAttribute("message","登陆成功！");
             req.getRequestDispatcher("/success.jsp").forward(req,resp);
         }
     }
