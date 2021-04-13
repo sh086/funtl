b386c59b72ec0b3997a2e7fd38ea8326551622db
suhe <suheforvip@gmail.com>
2021-04-13 22:44:29 +0800

    add:重命名文档名称

--- docs/.vuepress/nav.js
+++ docs/.vuepress/nav.js
@@ -1,19 +1,8 @@
 module.exports = [
     {text: '博客', link: '/blog/'},
-    {
-        text: '指南',
-        items: [
-            {text: '鲁斯菲尔-李卫民', link: '/guide/funtl/'},
-        ]
-    },
-    {text: '项目', link: ''},
-    {text: '工作流', link: ''},
-    {
-        text: 'Work',
-        items: [
-            {text: 'Uploader', link: '/work/uploader/'},
-        ]
-    },
+    {text: '指南', link: '/guide/funtl/'},
+    {text: '项目', link: '/project/'},
+    {text: '工作流', link: '/flow/'},
     {
         text: '在线文档',
         items: [
